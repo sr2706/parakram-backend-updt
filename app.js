196af46d38d38654dfa494673d10138771ac8d13
Sai Rugved <105142163+sr2706@users.noreply.github.com>
2025-03-09 16:48:22 +0530

    added cors hosting

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ connectDB();
 
 // Initialize app
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*', // Or specify allowed origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
